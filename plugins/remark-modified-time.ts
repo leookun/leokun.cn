5a331b8634ae89fc5c893c953b916d8f19b28aed
leokun <leokun@leokundeMac-Pro.local>
2024-10-15 13:15:12 +0800

    release

--- plugins/remark-modified-time.ts
+++ plugins/remark-modified-time.ts
@@ -1,10 +1,13 @@
 import { statSync } from "fs";
-
+import dayjs from "dayjs";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
     const result = statSync(filepath);
     file.data.astro.frontmatter.pubDatetime = result.birthtime.toISOString();
     file.data.astro.frontmatter.modDatetime = result.mtime.toISOString();
+    file.data.astro.frontmatter.modifiedText = dayjs(file.data.astro.frontmatter.modDatetime).format("YYYY-MM-DD HH:mm:ss");
+    file.data.astro.frontmatter.pubTimeText = dayjs(file.data.astro.frontmatter.pubDatetime).format("YYYY-MM-DD HH:mm:ss");
+
   };
 }
--- src/utils/getGroups.ts
+++ src/utils/getGroups.ts
@@ -1,41 +1,32 @@
-import { getCollection, type CollectionEntry } from "astro:content";
+import { getCollection } from "astro:content";
 // 只生成这个文件夹下的文章
-const FOLDER = "leokun.cn";
 import getSortedPosts from "./getSortedPosts";
 /*获取首页集合 */
 export default async () => {
-  const groups = new Map<string, Record<string, CollectionEntry<"bolg">>>();
+  const newPost=[]
   const posts = await getCollection("bolg");
   for (const post of posts) {
-    const rootFolder = post.id.split("/")?.[0];
-    const secondFolder = post.id.split("/")?.[1];
-    const displayName = post.id
+
+    const secondFolder = post.slug.split("/")?.[1];
+    const displayName = post.slug
       .split("/")
       ?.slice(2)
       .join("/")
       ?.replace(".md", "");
-    if (rootFolder === FOLDER && secondFolder && displayName) {
-      const res = await post.render();
-      const normalizedPost = {
-        ...post,
-        displayName,
-        data: {
-          title: displayName,
-          
-          ...post.data,
-          ...res.remarkPluginFrontmatter,
-        },
-      };
-      if (!groups.has(secondFolder)) {
-        groups.set(secondFolder, [normalizedPost]);
-      } else {
-        groups.get(secondFolder)?.push(normalizedPost);
-      }
-    }
+    const res = await post.render();
+    const normalizedPost = {
+      ...post,
+      displayName,
+      collection: secondFolder,
+      data: {
+        title: displayName,
+        ...post.data,
+        ...res.remarkPluginFrontmatter,
+      },
+    };
+    newPost.push(normalizedPost);
   }
-  // 对每个分组进行排序
-  for (const [key, value] of groups) {
-    groups.set(key, getSortedPosts(value));
-  }
-  return groups;
+  const sortedPosts = getSortedPosts(newPost);
+
+  return sortedPosts;
 };
